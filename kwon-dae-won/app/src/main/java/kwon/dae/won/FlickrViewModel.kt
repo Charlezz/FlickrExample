db39e043aef5f23a43c238c5989fe3bb2bcd8e6d
jayden.d <jayden.d@kakaocorp.com>
2023-02-10 02:57:07 +0900

    feat : RemoteMediator 페이징 구현
    TODO : LazyHorizontalGrid 구현

--- kwon-dae-won/app/src/main/java/kwon/dae/won/FlickrViewModel.kt
+++ kwon-dae-won/app/src/main/java/kwon/dae/won/FlickrViewModel.kt
@@ -7,19 +7,47 @@ import android.os.Environment
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import kwon.dae.won.data.retrofit.FlickrRetrofitFactory
+import kwon.dae.won.data.room.PhotosDatabase
+import kwon.dae.won.data.room.PhotosRemoteMediator
 import kwon.dae.won.data.usecase.DownloadUseCase
+import kwon.dae.won.domain.model.Photo
+import kwon.dae.won.domain.repository.FlickrRepository
 import kwon.dae.won.domain.usecase.GetRecentUseCase
 import timber.log.Timber
 import javax.inject.Inject
 
+const val PAGE_SIZE = 100
 @HiltViewModel
 class FlickrViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     private val getRecentUseCase: GetRecentUseCase,
-    private val downloadUseCase: DownloadUseCase
+    private val downloadUseCase: DownloadUseCase,
+    private val flickrRepository: FlickrRepository,
+    private val photosRemoteMediator: PhotosRemoteMediator,
+    private val photosDatabase: PhotosDatabase,
 ) : ViewModel() {
 
+    @OptIn(ExperimentalPagingApi::class)
+    fun getRecentPhotos(): Flow<PagingData<Photo>> =
+        Pager(
+            config = PagingConfig(
+                pageSize = PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                photosDatabase.getPhotosDao().getPhotos()
+            },
+            remoteMediator = PhotosRemoteMediator(
+                flickrRepository,
+                photosDatabase,
+            )
+        ).flow
 }
