40ce9322738cc74d696a00d97249582214296694
KwonDae <green201402317@gmail.com>
2023-02-10 17:55:26 +0900

    feat : AlertDialog & 다운로드 기능 구현

--- kwon-dae-won/app/src/main/java/kwon/dae/won/FlickrActivity.kt
+++ kwon-dae-won/app/src/main/java/kwon/dae/won/FlickrActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.paging.compose.collectAsLazyPagingItems
 import dagger.hilt.android.AndroidEntryPoint
+import kwon.dae.won.ui.DefaultAlertDialog
 import kwon.dae.won.ui.FlickrApp
+import kwon.dae.won.ui.imageUrl
 
 @AndroidEntryPoint
 class FlickrActivity : AppCompatActivity() {
@@ -16,8 +22,34 @@ class FlickrActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val photos = viewModel.getRecentPhotos().collectAsLazyPagingItems()
-            FlickrApp(photos)
+            val lazyPagingPhotos = viewModel.getRecentPhotos().collectAsLazyPagingItems()
+            val photos by remember { mutableStateOf(lazyPagingPhotos) }
+            var openDialog by remember { mutableStateOf(false to -1) }
+
+            FlickrApp(
+                photos = photos,
+                onLongClick = { index ->
+                    openDialog = true to index
+                }
+            )
+
+            if (openDialog.first) {
+                DefaultAlertDialog(
+                    onDismissButtonClick = { openDialog = false to -1 },
+                    onConfirmButtonClick = {
+                        if (openDialog.second > 0) {
+                            photos[openDialog.second]?.let { item ->
+                                viewModel.downloadPhoto(
+                                    item.title,
+                                    item.owner,
+                                    imageUrl(item.id, item.secret)
+                                )
+                            }
+                        }
+                        openDialog = false to -1
+                    }
+                )
+            }
         }
     }
 }
