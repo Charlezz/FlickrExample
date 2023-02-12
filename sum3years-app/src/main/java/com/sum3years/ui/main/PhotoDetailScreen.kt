dfdadacf3e56120cf79f36d47f7ce0ee5ca3d98a
mangbaam <pmb0836@gmail.com>
2023-02-13 06:18:02 +0900

    style: 닫기 버튼 뒤에 반투명 배경 추가하여 가시성 높임

--- sum3years-app/src/main/java/com/sum3years/ui/main/PhotoDetailScreen.kt
+++ sum3years-app/src/main/java/com/sum3years/ui/main/PhotoDetailScreen.kt
@@ -17,6 +17,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -57,6 +59,13 @@ fun PhotoDetail(
                 Icon(
                     imageVector = Icons.Default.Close,
                     contentDescription = "close photo detail screen",
+                    modifier = Modifier
+                        .drawBehind {
+                            drawCircle(
+                                color = Color.White.copy(alpha = .5f),
+                                radius = size.maxDimension,
+                            )
+                        },
                 )
             }
         }
