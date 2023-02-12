308dede117f7cff5ecc64a8d752ce1babf873855
mangbaam <pmb0836@gmail.com>
2023-02-13 06:11:42 +0900

    feat: 사진 누르면 다운로드 상세 화면 표시

--- sum3years-app/src/main/java/com/sum3years/MainActivity.kt
+++ sum3years-app/src/main/java/com/sum3years/MainActivity.kt
@@ -5,24 +5,32 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sum3years.data.datasource.FlickerRemoteDataSource
 import com.sum3years.data.network.FlickerService
 import com.sum3years.domain.repository.FlickerRepositoryImpl
+import com.sum3years.model.PhotoUIModel
 import com.sum3years.ui.main.MainScreen
-import com.sum3years.ui.theme.FlickrExampleTheme
+import com.sum3years.ui.main.PhotoDetail
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,6 +48,8 @@ class MainActivity : ComponentActivity() {
             }
             val errorMessage = viewModel.errorMessage.collectAsState()
 
+            var showPhotoDetail by remember { mutableStateOf<PhotoUIModel?>(null) }
+
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background,
@@ -52,12 +62,7 @@ class MainActivity : ComponentActivity() {
                         ),
                         viewModel = viewModel,
                     ) { photo ->
-                        // TODO Download Image
-                        Toast.makeText(
-                            this,
-                            "Download ${photo.loadUrlOriginal}",
-                            Toast.LENGTH_SHORT,
-                        ).show()
+                        showPhotoDetail = photo
                     }
                     if (errorMessage.value.isNotBlank()) {
                         Snackbar {
@@ -66,19 +71,28 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+            showPhotoDetail?.let { photo ->
+                Dialog(onDismissRequest = { showPhotoDetail = null }) {
+                    Surface(
+                        modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+                        shape = RoundedCornerShape(8.dp),
+                        color = MaterialTheme.colorScheme.background,
+                    ) {
+                        PhotoDetail(
+                            photo = photo,
+                            onClose = { showPhotoDetail = null },
+                            onDownloadClick = {
+                                // TODO Download Image
+                                Toast.makeText(
+                                    this,
+                                    "Download ${photo.loadUrlOriginal}",
+                                    Toast.LENGTH_SHORT,
+                                ).show()
+                            },
+                        )
+                    }
+                }
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FlickrExampleTheme {
-        Greeting("Android")
     }
 }
---
+++ sum3years-app/src/main/java/com/sum3years/ui/main/PhotoDetailScreen.kt
@@ -0,0 +1,86 @@
+package com.sum3years.ui.main
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.sum3years.model.PhotoUIModel
+import com.sum3years.ui.theme.FlickrExampleTheme
+
+@Composable
+fun PhotoDetail(
+    photo: PhotoUIModel,
+    modifier: Modifier = Modifier,
+    onDownloadClick: () -> Unit,
+    onClose: () -> Unit,
+) {
+    Column(
+        modifier = modifier
+            .wrapContentHeight()
+            .padding(bottom = 30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+        ) {
+            AsyncImage(
+                model = photo.loadUrlMedium,
+                contentDescription = "Photo detail : ${photo.title}",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            IconButton(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.TopEnd),
+                onClick = { onClose() },
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "close photo detail screen",
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = photo.title,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(horizontal = 16.dp),
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(onClick = { onDownloadClick() }) {
+            Text(text = "다운로드")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotoDetailPreview() {
+    FlickrExampleTheme {
+        PhotoDetail(photo = PhotoUIModel(), onDownloadClick = {}) {}
+    }
+}
