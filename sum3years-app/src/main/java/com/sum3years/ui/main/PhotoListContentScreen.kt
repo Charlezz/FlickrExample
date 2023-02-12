dcc289777c7f004ae1d58ba480bd04d1acba129a
mangbaam <pmb0836@gmail.com>
2023-02-13 02:08:01 +0900

    style: 사진을 여백 없이 채우는 UI로 변경

--- sum3years-app/src/main/java/com/sum3years/ui/main/PhotoListContentScreen.kt
+++ sum3years-app/src/main/java/com/sum3years/ui/main/PhotoListContentScreen.kt
@@ -1,29 +1,27 @@
 package com.sum3years.ui.main
 
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.sum3years.model.PhotoUIModel
@@ -40,6 +38,8 @@ fun PhotoListContent(
     val scrollState = rememberLazyGridState(itemCount)
     var loadedItems = 0
 
+    val minSize: Dp = 100.dp
+
     Surface(
         modifier = modifier.fillMaxSize(),
         color = Color(0xffEEEEEE),
@@ -51,14 +51,13 @@ fun PhotoListContent(
         // List of Photos
         LazyVerticalGrid(
             modifier = Modifier.fillMaxSize(),
-            columns = GridCells.Adaptive(minSize = 150.dp),
+            columns = GridCells.Adaptive(minSize = minSize),
             state = scrollState,
             contentPadding = WindowInsets.systemBars
                 .only(WindowInsetsSides.Bottom)
                 .add(
                     WindowInsets(left = 8.dp, right = 8.dp, top = 16.dp, bottom = 16.dp),
                 ).asPaddingValues(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             itemsIndexed(
                 items = photoList,
@@ -67,114 +66,15 @@ fun PhotoListContent(
                 },
             ) { index: Int, photo: PhotoUIModel ->
                 loadedItems = maxOf(loadedItems, index + 1)
-                Box(modifier = Modifier.defaultMinSize(150.dp, 150.dp).fillMaxSize()) {
+                Box(modifier = Modifier.width(minSize).height(minSize).fillMaxSize()) {
                     AsyncImage(
                         model = photo.loadUrlSmall,
                         contentDescription = null,
-                        modifier = Modifier.clickable { onClick(photo) },
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize().clickable { onClick(photo) },
                     )
                 }
             }
         }
     }
-}
-
-@Composable
-fun PhotoListContentMock(
-    modifier: Modifier = Modifier,
-    photoList: List<PhotoUIModel>,
-    onClick: (PhotoUIModel) -> Unit,
-    endOfList: (Boolean) -> Unit,
-) {
-    val itemCount = photoList.size
-    val scrollState = rememberLazyGridState(itemCount)
-    var loadedItems = 0
-
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = Color(0xffEEEEEE),
-    ) {
-        // Reached the end
-        if (loadedItems == itemCount && !scrollState.isScrollInProgress) {
-            endOfList(true)
-        }
-        // List of Photos
-        LazyVerticalGrid(
-            modifier = Modifier.fillMaxSize(),
-            columns = GridCells.Adaptive(minSize = 300.dp),
-            state = scrollState,
-            contentPadding = WindowInsets.systemBars
-                .only(WindowInsetsSides.Bottom)
-                .add(
-                    WindowInsets(left = 8.dp, right = 8.dp, top = 16.dp, bottom = 16.dp),
-                ).asPaddingValues(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-            itemsIndexed(
-                items = photoList,
-                key = { _: Int, item: PhotoUIModel ->
-                    item.id
-                },
-            ) { index: Int, photo: PhotoUIModel ->
-                loadedItems = maxOf(loadedItems, index + 1)
-                Box(
-                    modifier = Modifier.clickable {
-                        onClick(photo)
-                    },
-                ) {
-                    Image(imageVector = Icons.Default.Person, contentDescription = null)
-                }
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PhotoListContentPreview() {
-    val photos = listOf(
-        PhotoUIModel(
-            id = "52417436827",
-            owner = "72988954@N00",
-            secret = "52b528c896",
-            server = "65535",
-            title = "Park City Mammoth Cave KY 1940s Spelunking CRYSTAL LAKE BOAT TOUR @ ONYX Mammoth Cave National Park A Major Flint Ridge Cave Edmund Turner & LP Edwards History1",
-        ),
-        PhotoUIModel(
-            id = "52644426954",
-            owner = "72988954@N00",
-            secret = "3a9fe9432c",
-            server = "65535",
-            title = "SHIP Houghton Hancock MI c.1915 Unusual Barrels of Copper & Ingots to load @ Steamer Freighter Docks & Copper Range RR Yards RED JACKET CALUMET HECLA DOLLAR BAY Smelted Ingots Portage Lake KEWEENAW HISTORY1",
-        ),
-        PhotoUIModel(
-            id = "52417436827",
-            owner = "72988954@N00",
-            secret = "52b528c896",
-            server = "65535",
-            title = "Park City Mammoth Cave KY 1940s Spelunking CRYSTAL LAKE BOAT TOUR @ ONYX Mammoth Cave National Park A Major Flint Ridge Cave Edmund Turner & LP Edwards History1",
-        ),
-        PhotoUIModel(
-            id = "52644426954",
-            owner = "72988954@N00",
-            secret = "3a9fe9432c",
-            server = "65535",
-            title = "SHIP Houghton Hancock MI c.1915 Unusual Barrels of Copper & Ingots to load @ Steamer Freighter Docks & Copper Range RR Yards RED JACKET CALUMET HECLA DOLLAR BAY Smelted Ingots Portage Lake KEWEENAW HISTORY1",
-        ),
-        PhotoUIModel(
-            id = "52417436827",
-            owner = "72988954@N00",
-            secret = "52b528c896",
-            server = "65535",
-            title = "Park City Mammoth Cave KY 1940s Spelunking CRYSTAL LAKE BOAT TOUR @ ONYX Mammoth Cave National Park A Major Flint Ridge Cave Edmund Turner & LP Edwards History1",
-        ),
-        PhotoUIModel(
-            id = "52644426954",
-            owner = "72988954@N00",
-            secret = "3a9fe9432c",
-            server = "65535",
-            title = "SHIP Houghton Hancock MI c.1915 Unusual Barrels of Copper & Ingots to load @ Steamer Freighter Docks & Copper Range RR Yards RED JACKET CALUMET HECLA DOLLAR BAY Smelted Ingots Portage Lake KEWEENAW HISTORY1",
-        ),
-    )
-    PhotoListContentMock(photoList = photos, onClick = {}, endOfList = {})
 }
