a067be0e1c6285ab5d2313cb670ddd23a7ef796e
mangbaam <pmb0836@gmail.com>
2023-02-22 17:40:49 +0900

    feat: 롱클릭으로 사진 삭제

--- sum3years-app/src/main/java/com/sum3years/ui/main/DownloadedScreen.kt
+++ sum3years-app/src/main/java/com/sum3years/ui/main/DownloadedScreen.kt
@@ -1,25 +1,37 @@
 package com.sum3years.ui.main
 
 import android.os.Environment
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import java.io.File
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DownloadedScreen() {
     val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
     val images = downloadDir.listFiles { file -> file.isFile && file.extension == "jpg" }
+    var deleteTarget: File? by remember { mutableStateOf(null) }
 
     if (images != null && images.isNotEmpty()) {
         LazyColumn(
@@ -27,11 +39,17 @@ fun DownloadedScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             items(images.size) { index ->
+                val imageFile = images[images.lastIndex - index]
                 AsyncImage(
-                    model = images[images.lastIndex - index],
+                    model = imageFile,
                     contentDescription = "downloaded image",
-                    modifier = Modifier.fillMaxWidth(),
                     contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxWidth().combinedClickable(
+                        onClick = { },
+                        onLongClick = {
+                            deleteTarget = imageFile
+                        },
+                    ),
                 )
             }
         }
@@ -48,5 +66,33 @@ fun DownloadedScreen() {
                 color = MaterialTheme.colorScheme.primary,
             )
         }
+    }
+
+    deleteTarget?.let { file ->
+        AlertDialog(
+            onDismissRequest = { deleteTarget = null },
+            title = {
+                Text(text = "정말 삭제하시겠습니까?")
+            },
+            text = {
+                Text(text = "저장한 사진이 삭제됩니다.")
+            },
+            confirmButton = {
+                TextButton(onClick = {
+                    file.delete()
+                    deleteTarget = null
+                }) {
+                    Text("삭제")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    deleteTarget = null
+                }) {
+                    Text("취소")
+                }
+            },
+            shape = RoundedCornerShape(8.dp),
+        )
     }
 }
